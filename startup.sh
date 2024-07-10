945a082564b8faa11563cd4e6041fcd711d7e077
chrismaster <chrismaster360@gmail.com>
2024-07-10 15:27:36 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ ip route add local 0.0.0.0/0 dev lo table 123
 ip -6 rule add from ::1/128 iif lo table 123
 ip -6 route add local ::/0 dev lo table 123
 
-/go/go-mmproxy -l 127.0.0.1:$in_port -6 [::1]:$out_port -4 127.0.0.1:$out_port -v $logs -allowed-subnets /conf/allowed-subnets.txt
+/go/go-mmproxy -l 0.0.0.0:$in_port -6 [::1]:$out_port -4 127.0.0.1:$out_port -v $logs -allowed-subnets /conf/allowed-subnets.txt
